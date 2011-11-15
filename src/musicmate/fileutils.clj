c9bca15083853791683328b39686d9f7a34368f8
Joe DAlessandro <joe@thejoeshow.net>
2011-11-14 22:03:17 -0500

    some adjustments to file utils

--- src/musicmate/fileutils.clj
+++ src/musicmate/fileutils.clj
@@ -7,28 +7,29 @@
 ;; probably a better way to do this...
 
 ;; creates a reader for the file
-(defn get_reader [filename]
-  (java.io.BufferedReader. (java.io.FileReader. filename)))
+(defn getIstream [filename]
+  (java.io.FileInputStream. (java.io.File. filename)))
 
 ;; reads one line at a time
 ;; prints each line
-(defn readlines [reader]
-  (let [line (.readLine reader)]
+(defn readlines [istream]
+  (let [line (.readLine istream)]
     (when (not (nil? line))
       (println line)
-      (recur reader))))
+      (recur istream))))
 
 ;; helper function for to
 ;; accumulate list of bytes
-(defn rxHlp [reader x acc pos]
-  (if (< pos x)
-    (recur reader x
-           (conj acc (.read reader))
-           (inc pos))
-    (reverse acc)))
+(defn rxHlp [istream x acc pos]
+  (let [byte (.read istream)]
+    (if (and (pos? byte) (< pos x))
+      (recur istream x
+             (conj acc byte)
+             (inc pos))
+      (reverse acc))))
 
 ;; reads x bytes
 ;; with offset y
-(defn readXBytes [reader x y]
-  (.skip reader y)
-  (rxHlp reader x '() 0))
+(defn readX [istream x y]
+  (.skip istream y)
+  (rxHlp istream x '() 0))
