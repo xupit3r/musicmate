6b8df08f3b21d2ee1c28277a3b66421d99842f63
Joe DAlessandro <joe@thejoeshow.net>
2011-11-16 20:19:19 -0500

    added bit functions. altered readlines to return a list of lines.

--- src/musicmate/fileutils.clj
+++ src/musicmate/fileutils.clj
@@ -6,17 +6,22 @@
 ;; the underlying Java file utilities.  there is
 ;; probably a better way to do this...
 
-;; creates a reader for the file
+;; returns an input stream
+;; for a file
 (defn getIstream [filename]
   (java.io.FileInputStream. (java.io.File. filename)))
 
-;; reads one line at a time
-;; prints each line
-(defn readlines [istream]
+;; helper function for readlines
+(defn rlHlp [istream acc]
   (let [line (.readLine istream)]
-    (when (not (nil? line))
-      (println line)
-      (recur istream))))
+    (if (not (nil? line))
+      (recur istream (conj acc line))
+      (reverse acc))))
+
+;; reads each line of a
+;; file into a list
+(defn readlines [filename]
+  (rlHlp (getIstream filename) '()))
 
 ;; helper function for to
 ;; accumulate list of bytes
@@ -30,6 +35,34 @@
 
 ;; reads x bytes
 ;; with offset y
-(defn readX [istream x y]
-  (.skip istream y)
-  (rxHlp istream x '() 0))
+(defn readX [filename x y]
+  (let [istream (getIstream filename)]
+    (.skip istream y)
+    (rxHlp istream x '() 0)))
+
+;;;; Stuff to deal with bits and bytes ;;;;
+
+(defn getbit [byte bit]
+  (if (> (bit-and
+          (bit-shift-right byte bit) 1)
+         0) 1 0))
+
+;; bitme helper
+(defn bmHlp [byte bit lst]
+  (let [rslt (getbit byte bit)]
+    (if (< bit 8)
+      (recur byte (inc bit) (conj lst rslt))
+      (reverse lst))))
+
+;; turn a byte list into a bit
+;; list. note the least significant
+;; bit will be on the left and not
+;; the right
+(defn bitme [byte]
+  (bmHlp byte 0 '()))  
+
+;; takes a list of bytes and
+;; returns a list of bits that
+;; represent the bytes
+(defn getbits [bytes]
+  (map bitme bytes))
